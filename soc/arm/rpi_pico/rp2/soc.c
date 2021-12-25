61243c7fa6ee4b8b4f9330117cd3cb1c1315c450
Peter Johanson <peter@peterjohanson.com>
2021-12-24 23:13:11 -0500

    soc: rpi_pico: Add restart to bootloader support.
    Reboot to the bootloader if non-zero restart type.
    
    Signed-off-by: Peter Johanson <peter@peterjohanson.com>

--- soc/arm/rpi_pico/rp2/soc.c
+++ soc/arm/rpi_pico/rp2/soc.c
@@ -15,6 +15,8 @@
 
 #include <stdio.h>
 
+#include <cmsis_core.h>
+
 #include <zephyr/kernel.h>
 #include <zephyr/init.h>
 #include <zephyr/logging/log.h>
@@ -23,8 +25,38 @@
 #include <hardware/regs/resets.h>
 #include <hardware/clocks.h>
 #include <hardware/resets.h>
+#include <pico/bootrom.h>
 
 LOG_MODULE_REGISTER(soc, CONFIG_SOC_LOG_LEVEL);
+
+/* Overrides the weak ARM implementation:
+   Set general purpose retention register and reboot */
+void sys_arch_reboot(int type)
+{
+	if (type != 0) {
+		reset_usb_boot(0,0);
+	} else {
+		NVIC_SystemReset();
+	}
+}
+
+static int rp2040_init(void)
+{
+	reset_block(~(RESETS_RESET_IO_QSPI_BITS | RESETS_RESET_PADS_QSPI_BITS |
+		      RESETS_RESET_PLL_USB_BITS | RESETS_RESET_PLL_SYS_BITS));
+
+	unreset_block_wait(RESETS_RESET_BITS &
+			   ~(RESETS_RESET_ADC_BITS | RESETS_RESET_RTC_BITS |
+			     RESETS_RESET_SPI0_BITS | RESETS_RESET_SPI1_BITS |
+			     RESETS_RESET_UART0_BITS | RESETS_RESET_UART1_BITS |
+			     RESETS_RESET_USBCTRL_BITS | RESETS_RESET_PWM_BITS));
+
+	clocks_init();
+
+	unreset_block_wait(RESETS_RESET_BITS);
+
+	return 0;
+}
 
 /*
  * Some pico-sdk drivers call panic on fatal error.
